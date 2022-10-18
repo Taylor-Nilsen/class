45c1da03cf6e7c74b466972b30e76229fa070e34
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-10-18 10:16:03 -0700

    1

---
+++ cpp/Asignment1.cpp
@@ -0,0 +1,18 @@
+/*
+Taylor Nilsen
+ID: 0702201
+October 18, 2022
+This is Asignment 1 and it will output my personal information to the console
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+/*cout 
+First Name: Taylor
+Last Name: Nilsen
+Favorite Movie: Catch Me If You Can
+Favorite Hobby: Astronomy
+*/
