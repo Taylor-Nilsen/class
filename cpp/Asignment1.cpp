9016ef00253abaa6ba156209b3794f6f4f88ee6e
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-10-19 13:09:32 -0700

    1

--- cpp/Asignment1.cpp
+++ cpp/Asignment1.cpp
@@ -8,13 +8,16 @@ This is Asignment 1 and it will output my personal information to the console
 
 using namespace std;
 
-int main() {
-    cout << "First Name: Taylor" << endl;
-    cout << "Last Name: Nilsen" << endl;
-    cout << "Favorite Movie: Catch Me If You Can" << endl;
-    cout << "Favorite Hobby: Astronomy" << endl;
-    return 0;
+int
+main ()
+{
+  cout << "First Name: Taylor" << endl << endl;
+  cout << "Last Name: Nilsen" << endl << endl;
+  cout << "Favorite Movie: Catch Me If You Can" << endl << endl;
+  cout << "Favorite Hobby: Astronomy" << endl << endl;
+  return 0;
 }
+
 /*cout 
 First Name: Taylor
 Last Name: Nilsen
