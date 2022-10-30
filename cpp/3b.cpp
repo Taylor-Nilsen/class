1aae2e21668670268d6629d50731d9205ee96e14
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-10-30 00:24:59 -0700

    hjbm mnb nhmg

---
+++ cpp/3b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int score;
+
+    cout << "enter score" << endl;
+    cin >> score;
+
+    if (score >= 90) {
+        cout << "You got and A" << endl;
+    }
+
+    else if (score >= 80) {
+        cout << "You got and B" << endl;
+    }
+
+    else if (score >= 70) {
+        cout << "You got and C" << endl;
+    }
+
+    else if (score >= 60) {
+        cout << "You got and D" << endl;
+    }
+
+    else {
+        cout << "You got and D" << endl;
+    }
+
+    return 0;
+}
