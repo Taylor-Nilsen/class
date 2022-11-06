fd6ada532a2f1247b73864dc61c7404369d03144
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-06 13:18:47 -0800

    dsegrf

---
+++ cpp/quiz1.cpp
@@ -0,0 +1,18 @@
+/*
+Taylor Nilsen
+ID: 0702201
+Nov 6 2022
+Quiz 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    char myChar;
+    int increment;
+
+    cout << "Enter a character" << endl;
+    cin >> myChar >> endl;
+    
+}
