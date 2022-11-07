f2ab0e16cb5d7dcad5ead530e83054742d3c229c
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-06 16:01:13 -0800

    4terttge

---
+++ cpp/Assigmnets/6.cpp
@@ -0,0 +1,24 @@
+/*
+Taylor Nilsen
+ID: 0702201
+October 29, 2022
+7b
+Design and create algorithms for solving simple problems.
+*/
+
+#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int num, num0;
+    char op;
+    cout << "Input a Calculation" << endl;
+    cin >> num >> op >> num0;
+
+    cout << num op num0;
+
+
+    return 0;
+}
