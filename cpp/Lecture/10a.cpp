97f7766c4c203f1bfc40c1b9a9648c3bbf6396c5
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-20 11:49:51 -0800

    effew

--- cpp/Lecture/10a.cpp
+++ cpp/Lecture/10a.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 int getval(), sqrval(), cubval();
 using namespace std;
-double Value, Number, sqrt, cub;
+double Value, Number1, Number2, square, cube;
 
 int main(){
-    int Number = getValue();
-    cout << number;
-
-    return "main";
+    sqrval() = square
+    cubval() = cube
+    cout << "Your Number Squared is: " << square << endl;
+    cout << "Your Number Cubed is: " << cube << endl;
+    return 0;
 }
 
 int getval(){
     cout << "Enter a Value: ";
     cin >> Value;
-    return "Value";
+    return Value;
+}
+
+int sqrval(){
+    Number1 = getval();
+    return Number * Number;
+}
+int cubval(){
+    Number2 = getval();
+    return Number * Number * Number;
 }
