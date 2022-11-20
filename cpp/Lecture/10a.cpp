781442d8debcccb2f2d3914696288b87975fc855
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-20 11:26:14 -0800

    fhthfghftr

---
+++ cpp/Lecture/10a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int getval(), sqrval(), cubval();
+using namespace std;
+double Value, Number, sqrt, cub;
+
+int main(){
+    int Number = getValue();
+    cout << number;
+
+    return "main";
+}
+
+int getval(){
+    cout << "Enter a Value: ";
+    cin >> Value;
+    return "Value";
+}
