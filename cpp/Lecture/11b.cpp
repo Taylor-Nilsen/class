ac0eefe6d2facb8d0f4b29e5a6ffbda2b7fe45f6
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-20 18:53:29 -0800

    efw

--- cpp/Lecture/11b.cpp
+++ cpp/Lecture/11b.cpp
@@ -1,6 +1,14 @@
+/*
+Taylor Nilsen
+0702201
+Lecture 11b
+20 November 2022
+Decompose a program into subtasks and use parameter passing to exchange information between the subparts
+*/
 #include <iostream>
 using namespace std; 
 void getInts(int& a , int & b);
+void sort(int& a, int& b, bool descend = true)
 void SWAP(int& a , int & b);
 
 int main(){
@@ -8,9 +16,16 @@ int main(){
     getInts(x, y);
 
     cout << " X : "<< x << "\t and Y: " << y<< endl;\
+    
     SWAP(x,y);
     cout << "Swapped they are\t X : "<< x << "\t and Y: " << y<< endl;\
-
+    
+    sort(x,y);
+    cout << "sorted decending they are\t X : "<< x << "\t and Y: " << y<< endl;\
+    
+    sort(x,y, false);
+    cout << "sorted ascending they are\t X : "<< x << "\t and Y: " << y<< endl;\
+    
     return 0;
 }
 
@@ -25,4 +40,17 @@ void SWAP(int& a, int& b) {
     a=b;
     b=tmp;
     return;
+}
+
+void sort(int& a, int& b, bool descend) {
+    if(descend) {
+        if (a<b) {
+            SWAP(a,b);
+
+        }
+    } else {
+        if (a>b) {
+            SWAP(a,b);
+        }
+    }
 }
