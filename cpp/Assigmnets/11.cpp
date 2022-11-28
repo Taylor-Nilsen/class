3a0169e831e9922ecefa78a225eb4e12170880e4
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-11-27 19:20:33 -0800

    Create 11.cpp
    Jbk

---
+++ cpp/Assigmnets/11.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+int sumConsonants (char letter_1, char letter_2);
+bool isVowel(char c);
+
+int main(){
+    char first, second;
+    
+    cout << "Enter 2 Upper Case Letters" << endl;
+    cin >> first >> second;
+    int sum = sumConsonants (first, second);
+    
+    if (sum == -1){
+        cout << "error";
+    }
+        
+    else {
+        cout << "The consonants between your letters add to " << sum;
+    }
+    
+    
+    return 0;
+}
+
+int sumConsonants(char letter_1, char letter_2){
+    int sum = 0;
+    
+    char small = letter_1 < letter_2 ? letter_1 : letter_2;
+    char big = letter_1 > letter_2 ? letter_1 : letter_2;
+    
+    if(!islower(small) or islower(big))
+        return -1;
+    
+    for(char i = small; i <= big; i++){
+        if(!isVowel(i)){
+            sum+= static_cast<int>(i);
+        }
+    }
+    return sum;
+}
+
+bool isVowel(char c){
+    char ch = toupper(c);
+    
+    if(ch=='A' || ch=='U' || ch=='I' || ch=='O' || ch=='U'){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
