c608d5fd36b619ba4610ca350b1cbb9c0c191c3d
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-12-04 16:26:01 -0800

    gsdf

--- cpp/Lecture/13b.cpp
+++ cpp/Lecture/13b.cpp
@@ -1,24 +1,26 @@
 /*
 Taylor Nilsen
-0702201
-15
-4 December 2022
+ID: 0702201
+November 27, 2022
+13a
 Apply the program development process to problems that are solved using fundamental C++ programming constructs and predefined data structures.
 */
-
 #include <iostream>
 
 using namespace std;
 void fillArray(int ar[], int size);
 void printArray(const int ar[], int size);
+void printArrayBackwards(const int ar[], int size);
 
 int main(){
-    int ar[25];
+    int ar[15];
     
-    fillArray(ar, 25);
+    fillArray(ar, 15);
     
-    cout << printArray(ar, 25) << endl;
-        
+    printArray(ar, 15);
+    
+    printArrayBackwards(ar, 15);
+    
     return 0;
 }
 void fillArray (int ar[], int size) {
@@ -27,14 +29,14 @@ void fillArray (int ar[], int size) {
     }
     
 }
-int searchArray(const int ar[], int rSize){
-    int count = 0;
-    for (int row = 0; row < rSize; row++){
-        for (int col = 0; col < rSize; col++){
-            if ((ar[25])% 2 == 0){
-                count++;
-            }
-        }
+void printArray(const int ar[], int size){
+    for (int i=0; i < size; i++) {
+        cout << ar[i] << endl;
     }
-    return count;
+}
+
+void printArrayBackwards(const int ar[], int size){
+    for (int i=0; i > size; i--) {
+        cout << ar[i] << endl;
+    }
 }
