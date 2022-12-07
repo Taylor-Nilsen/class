3cedd76e3328a8bc8da00903e2f0a751f1d60442
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-12-07 19:26:59 +0000

    njdsk

--- cpp/Lecture/16b/16b.h
+++ cpp/Lecture/16b/16b.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+using std::string;
+
+class Player{
+private:
+    string name;
+    double average;
+public:
+    void setPlayerAverage(string name, double avg);
+    void printPlayerAverage();
+    string getName();
+};
+}
