6ecbf465057bdd9bba910332b8523250e7387b80
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-12-07 11:17:19 -0800

    bjk

--- cpp/Lecture/15b.cpp
+++ cpp/Lecture/15b.cpp
@@ -1,4 +1,22 @@
+/*
+Taylor Nilsen
+ID: 0702201
+7 December, 2022
+15b
+Apply the program development process to problems that are solved using fundamental C++ programming constructs and predefined data structures.
+*/
 #include <iostream>
+using namespace std;
+
+int main(){
+    int* ptr;
+    int ar[] = {3,2,3,4,5,7,12,10,9,2};
+    ptr = &ar[10];
+    for (int i = 0; i < 7; i++){
+        cout << *ptr-- << endl;
+    }
+    return 0;
+}#include <iostream>
 using namespace std;
 
 int xChar(char *str, char change);
