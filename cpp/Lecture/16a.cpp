173df8a9ba7ae5a1169d373fe512f31cff969b24
JohnBrown3 <115370232+JohnBrown3@users.noreply.github.com>
2022-12-06 17:56:59 -0800

    gefsrd

---
+++ cpp/Lecture/16a.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Player
+{
+    string name;
+    double average;
+};
+
+void getPlayerAverage(Player* plr);
+void printPlayerAverage(const Player& p);
+
+int main(){
+    Player p[3];
+    for (int i = 0; i < 3; i++){
+        getPlayerAverage(&p[i]);
+        printPlayerAverage(p[i]);
+    }
+    return 0;
+}
+void getPlayerAverage(Player* plr){
+    cout << "Enter player name: ";
+    getline(cin, plr->name);
+    cout << "Enter player average: ";
+    cin >> plr->average;
+}
+void printPlayerAverage(const Player& p){
+    cout << "Player name: " << p.name << endl;
+    cout << "Player average: " << p.average << endl;
+}
